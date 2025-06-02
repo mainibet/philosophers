3ec64d1a2f2ea37b29b3551fdd7952f1cdb5865b
Alicia Betancourt Mainieri <albetanc@c4a13c6.42berlin.de>
2025-06-02 10:24:26 +0200

    threads created

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 11:39:37 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/02 08:52:02 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/02 10:22:59 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,17 +109,76 @@ int	init_philo(t_program *data)
     //pending include destroy mutex for output and sim_over
 	return (SUCCESS);
 }
+long long	get_current_time_ms(void)//NEW
+{
+	struct timeval tv;
 
-// int	start_threads(t_program *data)
+	gettimeofday(&tv, NULL); // No timezone
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+// int set_initial_timestamps(t_program *program_data)//new
 // {
+//     int i;
 
+//     program_data->start_time = get_current_time_ms();
+//     if (program_data->start_time == 0) // Basic error check for time function (rare)
+//     {
+//         print_error_msg("Failed to get start time.");
+//         return (ERROR); // A generic error, or define a specific one if time functions can fail
+//     }
+
+//     i = 0;
+//     while (i < program_data->total_philo)
+//     {
+//         program_data->philo[i].last_meal = program_data->start_time;
+//         i++;
+//     }
+//     return (SUCCESS);
 // }
+
+//each will be a status
+//eating, thinking, and sleeping.
+//prototype given by pthread_create
+//void *(*start_routine)(void *), void *arg
+//thread logic
+void	*life_cycle(void *arg)
+{
+	t_philo *philo;//check if can be use this struct
+
+	philo = (t_philo *)arg;
+	printf("Philosopher %d is alive!\n", philo->philo_id);//test life cycle
+	sleep(1);  // Simulate some work
+    //pending to set a while loop with a cond to stop the loop term condition
+	// take_forks(philo);//function with pthread_mutex_lock. 
+    //check how to indicate take right and left if is odd or even
+	printf("Philosopher %d is done.\n", philo->philo_id);//test only thread creation
+	// eat(philo);//reacord time of last meal usleep(time_to_eat)
+	// release_fork(philo); ///function to unlock mutex needs to be per each fork
+	// sleep(philo); //usleep(time_to_sleep)
+	// think(philo);//dynamic how should be set?
+	return (NULL);
+}
+
+int	start_threads(t_program *data)
+{
+	// pthread_t	thread_id;
+	int	i;
+
+	i = 0;
+	while (i < data->total_philo)
+	{
+		pthread_create(&data->philo[i].thread_id, NULL, 
+			life_cycle, &data->philo[i]);
+		printf("Thread created for philo ID: %d\n", data->philo[i].philo_id);
+		i++;
+	}//after include a while loop to join threads then all will be working
+	return (SUCCESS);
+}
 
 int	setup_simulation(t_program *data)//check if **argv needed or only data?
 {
 	int	status;
 
-	// memset(data, 0, sizeof(t_program));
 	if (!data->end_flag)
 		data->end_flag = 0;//just to initialite the flag
 	status = init_cross_mutex(data);
@@ -131,9 +190,9 @@ int	setup_simulation(t_program *data)//check if **argv needed or only data?
 	status = init_philo(data);
 	if (status != SUCCESS)
 		return (status);//clean-up before return
-// 	status = start_threads(data);
-// 	if (status != SUCCESS)
-// 		return (status);//clean-up before return
+	status = start_threads(data);
+	if (status != SUCCESS)
+		return (status);//clean-up before return
 	return (SUCCESS);
 }
 
