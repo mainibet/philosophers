72360fff5eac8ddbe2223e06faa30c09eaeef100
Alicia Betancourt Mainieri <albetanc@c4a13c2.42berlin.de>
2025-06-06 07:29:15 +0200

    included in my main .h colors.h and macros.h

---
+++ philo/macros.h
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 07:26:13 by albetanc          #+#    #+#             */
+/*   Updated: 2025/06/06 07:27:19 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+# define MACROS_H
+
+// --- Maximum value long long ---
+# ifndef LLONG_MAX
+#  define LLONG_MAX 9223372036854775807LL
+# endif
+
+// --- Str long long conversion status ---
+# ifndef CONVERSION_SUCCESS
+#  define CONVERSION_SUCCESS 0
+# endif
+
+# ifndef CONVERSION_ERROR
+#  define CONVERSION_ERROR 1
+# endif
+
+# ifndef CONVERSION_OVERFLOW
+#  define CONVERSION_OVERFLOW 2
+# endif
+
+// --- Return Status Codes ---
+# ifndef SUCCESS
+#  define SUCCESS 0
+# endif
+
+# ifndef ERROR
+#  define ERROR -1
+
+# endif
+
+// --- Specific error codes if needed ---
+# ifndef ERR_INVALID_ARGS
+#  define ERR_INVALID_ARGS -2
+# endif
+# ifndef ERR_MALLOC_FAIL
+#  define ERR_MALLOC_FAIL -3
+# endif
+
+# ifndef ERR_MUTEX
+#  define ERR_MUTEX -4
+# endif
+
+# ifndef ERR_THREAD_CREAT//pending to use it
+#  define ERR_THREAD_CREATE -5
+# endif
+
+// --- Bolean values ---
+# ifndef FALSE//CHECK IF IS CORRECT AND NEEDED or if is in any lib
+#  define FALSE 0
+# endif
+
+# ifndef TRUE
+#  define TRUE 1
+# endif
+
+// --- Configuration values ---
+# ifndef MAX_MEALS_DISABLED
+#  define MAX_MEALS_DISABLED -1
+# endif
+
+# ifndef MUTEX_NO_INIT
+#  define MUTEX_NO_INIT 0
+# endif
+
+# ifndef MUTEX_INIT
+#  define MUTEX_INIT 1
+# endif
+
+# ifndef PHILO_ALIVED
+#  define PHILO_ALIVED 0
+# endif
+
+# ifndef PHILO_DIED
+#  define PHILO_DIED 1
+# endif
+
+#endif
--- philo/philo.h
+++ philo/philo.h
@@ -6,86 +6,87 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 11:39:53 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/05 11:48:26 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/06 07:28:42 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-// --- Maximum value long long ---
-# ifndef LLONG_MAX
-#  define LLONG_MAX 9223372036854775807LL
-# endif
+// // --- Maximum value long long ---
+// # ifndef LLONG_MAX
+// #  define LLONG_MAX 9223372036854775807LL
+// # endif
 
-// --- Str long long conversion status ---
-# ifndef CONVERSION_SUCCESS
-#  define CONVERSION_SUCCESS 0
-# endif
+// // --- Str long long conversion status ---
+// # ifndef CONVERSION_SUCCESS
+// #  define CONVERSION_SUCCESS 0
+// # endif
 
-# ifndef CONVERSION_ERROR
-#  define CONVERSION_ERROR 1
-# endif
+// # ifndef CONVERSION_ERROR
+// #  define CONVERSION_ERROR 1
+// # endif
 
-# ifndef CONVERSION_OVERFLOW
-#  define CONVERSION_OVERFLOW 2
-# endif
+// # ifndef CONVERSION_OVERFLOW
+// #  define CONVERSION_OVERFLOW 2
+// # endif
 
-// --- Return Status Codes ---
-# ifndef SUCCESS
-#  define SUCCESS 0
-# endif
+// // --- Return Status Codes ---
+// # ifndef SUCCESS
+// #  define SUCCESS 0
+// # endif
 
-# ifndef ERROR
-#  define ERROR -1
+// # ifndef ERROR
+// #  define ERROR -1
 
-# endif
+// # endif
 
-// --- Specific error codes if needed ---
-# ifndef ERR_INVALID_ARGS
-#  define ERR_INVALID_ARGS -2
-# endif
-# ifndef ERR_MALLOC_FAIL
-#  define ERR_MALLOC_FAIL -3
-# endif
+// // --- Specific error codes if needed ---
+// # ifndef ERR_INVALID_ARGS
+// #  define ERR_INVALID_ARGS -2
+// # endif
+// # ifndef ERR_MALLOC_FAIL
+// #  define ERR_MALLOC_FAIL -3
+// # endif
 
-# ifndef ERR_MUTEX
-#  define ERR_MUTEX -4
-# endif
+// # ifndef ERR_MUTEX
+// #  define ERR_MUTEX -4
+// # endif
 
-# ifndef ERR_THREAD_CREAT//pending to use it
-#  define ERR_THREAD_CREATE -5
-# endif
+// # ifndef ERR_THREAD_CREAT//pending to use it
+// #  define ERR_THREAD_CREATE -5
+// # endif
 
-// --- Bolean values ---
-# ifndef FALSE//CHECK IF IS CORRECT AND NEEDED or if is in any lib
-#  define FALSE 0
-# endif
+// // --- Bolean values ---
+// # ifndef FALSE//CHECK IF IS CORRECT AND NEEDED or if is in any lib
+// #  define FALSE 0
+// # endif
 
-# ifndef TRUE
-#  define TRUE 1
-# endif
+// # ifndef TRUE
+// #  define TRUE 1
+// # endif
 
-// --- Configuration values ---
-# ifndef MAX_MEALS_DISABLED
-#  define MAX_MEALS_DISABLED -1
-# endif
+// // --- Configuration values ---
+// # ifndef MAX_MEALS_DISABLED
+// #  define MAX_MEALS_DISABLED -1
+// # endif
 
-# ifndef MUTEX_NO_INIT
-#  define MUTEX_NO_INIT 0
-# endif
+// # ifndef MUTEX_NO_INIT
+// #  define MUTEX_NO_INIT 0
+// # endif
 
-# ifndef MUTEX_INIT
-#  define MUTEX_INIT 1
-# endif
+// # ifndef MUTEX_INIT
+// #  define MUTEX_INIT 1
+// # endif
 
-# ifndef PHILO_ALIVED
-#  define PHILO_ALIVED 0
-# endif
+// # ifndef PHILO_ALIVED
+// #  define PHILO_ALIVED 0
+// # endif
 
-# ifndef PHILO_DIED
-#  define PHILO_DIED 1
-# endif
+// # ifndef PHILO_DIED
+// #  define PHILO_DIED 1
+// # endif
+// --- STANDARD HEADERS ---
 
 # include <string.h> //memset CHECK IF USED
 # include <stdio.h> //printf
@@ -93,6 +94,11 @@
 # include <unistd.h> //usleep CHECK
 # include <sys/time.h> //gettimeofday
 # include <pthread.h> //for threads and mutex
+
+// --- MY HEADERS ---
+
+# include "macros.h"
+# include "colors.h"
 
 // -----------------------------------------//
 //                 STRUCTS                  //
