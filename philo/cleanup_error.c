da3f574ecb9ff24c68c3f2acfc3044ef9bb9cb0c
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-06-06 11:17:32 +0200

    refactored clean-up function

--- philo/cleanup_error.c
+++ philo/cleanup_error.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 11:02:26 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/06 11:12:56 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/06 11:17:14 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +31,76 @@ int	mutex_fork_error(t_program  *data, int i)//DOES THIS NEED TO UNLOCK IF SOMET
 	return (ERR_MUTEX);
 }//can this function work also for other mutex error?
 
+void	clean_forks(t_program *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->total_philo)
+	{
+		if (data->fork[i].fork_mut_status == MUTEX_INIT)
+			pthread_mutex_destroy(&data->fork[i].mutex);//need to includ error check of destroy?
+		i++;
+	}
+	// i = count_arr_elements((void **)data->fork);//need to include any handling error?
+	free (data->fork);
+	data->fork = NULL;//good practice after freeing
+	// free_array((void **)data->fork, i);
+}
+
+void	clean_philo(t_program *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->total_philo)
+	{
+		if (data->philo[i].mutex_status_phi == MUTEX_INIT)
+			pthread_mutex_destroy(&data->philo[i].philo_mutex);
+		i++;
+	}
+	free_array((void *)data->philo, data->total_philo);
+}
+
 //destroy mutex: output, fork, philo, monitor
 //free: array of philo
 //free:array of forks
 //free: array of parse
 void	clean_up_program(t_program *data)//check if philo dies if forks need to be "released"
 {//this function should be made by the main thread
-	int	i;//should I include a check in case the simulation is running first stop it?
+	int	i;
     //should I include the clean_error for forks?
-
+//should I include a check in case the simulation is running first stop it?
 	i = 0;
 	if (data->out_mut_status == MUTEX_INIT)
 		pthread_mutex_destroy(&data->output_mutex);//need to includ error check of destroy?
 	if (data->end_mutex_status == MUTEX_INIT)
 		pthread_mutex_destroy(&data->end_mutex);
 	if (data->fork != NULL)
-	{
-		i = 0;
-		while (i < data->total_philo)
-		{
-			if (data->fork[i].fork_mut_status == MUTEX_INIT)
-				pthread_mutex_destroy(&data->fork[i].mutex);//need to includ error check of destroy?
-			i++;
-		}
-		// i = count_arr_elements((void **)data->fork);//need to include any handling error?
-		free (data->fork);
-		data->fork = NULL;//good practice after freeing
-		// free_array((void **)data->fork, i);
-	}
+	// {
+		clean_forks(data);//new
+		// i = 0;
+		// while (i < data->total_philo)
+		// {
+		// 	if (data->fork[i].fork_mut_status == MUTEX_INIT)
+		// 		pthread_mutex_destroy(&data->fork[i].mutex);//need to includ error check of destroy?
+		// 	i++;
+		// }
+		// free (data->fork);
+		// data->fork = NULL;//good practice after freeing
+	// }
 	if (data->philo != NULL)
-	{
-		i = 0;
-		while (i < data->total_philo)
-		{
-			if (data->philo[i].mutex_status_phi == MUTEX_INIT)
-				pthread_mutex_destroy(&data->philo[i].philo_mutex);
-			i++;
-		}
-		free_array((void *)data->philo, data->total_philo);
-	}
+		clean_philo(data);
+	// {
+		// i = 0;
+		// while (i < data->total_philo)
+		// {
+		// 	if (data->philo[i].mutex_status_phi == MUTEX_INIT)
+		// 		pthread_mutex_destroy(&data->philo[i].philo_mutex);
+		// 	i++;
+		// }
+		// free_array((void *)data->philo, data->total_philo);
+	// }
 	if (data->parse != NULL && data->parse->arr != NULL)
 	{
 		i = count_arr_elements((void *)data->parse);
