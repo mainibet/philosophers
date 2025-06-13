fc2456746d01f2df7b7b928dcdb015fda539be27
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-06-13 08:59:51 +0200

    fix(life_cycle): included forks_taken into life_one_philo function

--- philo/life_cycle.c
+++ philo/life_cycle.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 07:49:31 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/13 08:55:07 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/13 08:58:11 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,10 +86,12 @@ void	*life_one_philo(void *arg)
 	philo->meal_number = 0;
 	pthread_mutex_unlock(&philo->philo_mutex);
 	pthread_mutex_lock(&philo->left_fork->fork_mutex);
+	philo->forks_taken = 1;//mark that was taken
 	print_status(philo, "has taken a fork");
 	while (check_end_cond(philo) == PHILO_ALIVED)
 		usleep(1000); // release CPU
 	pthread_mutex_unlock(&philo->left_fork->fork_mutex); // Libera el tenedor al morir/terminar
+	philo->forks_taken = 0;//reset
 	return (NULL);
 }
 
