52fc04ed7b531eaabbc4f9fb8b28b2b7aa81f47c
Alicia Betancourt Mainieri <albetanc@c4a7c8.42berlin.de>
2025-06-13 17:00:21 +0200

    refactor(output.c): printing error in stderr with write instead of printf included ft_strlen for that

--- philo/output.c
+++ philo/output.c
@@ -6,21 +6,11 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 07:35:32 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/12 14:48:43 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/13 16:59:17 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-// void	print_format(t_philo *philo, const char *msg)//will be used?
-// {
-// 	long long	current_time;
-
-// 	pthread_mutex_lock(&philo->program->start_mutex);
-// 	current_time = precise_time_ms() - philo->program->start_time;
-// 	printf("%lld %d %s\n", current_time, philo->philo_id, msg);
-// 	pthread_mutex_unlock(&philo->program->start_mutex);
-// }
 
 //HELPER FUNCTION
 //to print all the msg in order
@@ -37,7 +27,7 @@ void	print_status(t_philo *philo, const char *msg)//check if needs to handled an
 	long long	current_time;
 	int			sim_status;
 
-    pthread_mutex_lock(&philo->program->start_mutex);
+	pthread_mutex_lock(&philo->program->start_mutex);
 	current_time = precise_time_ms() - philo->program->start_time;
 	sim_status = check_end_cond(philo);
 	if (sim_status == PHILO_ALIVED)
@@ -46,11 +36,26 @@ void	print_status(t_philo *philo, const char *msg)//check if needs to handled an
 		printf("%lld %d %s\n", current_time, philo->philo_id, msg);
 		pthread_mutex_unlock(&philo->program->output_mutex);
 	}
-    pthread_mutex_unlock(&philo->program->start_mutex);//check if here or where
+	pthread_mutex_unlock(&philo->program->start_mutex);//check if here or where
 }
 
-//pending destroy mytex if an error occure after init mutex
-void	print_error_msg(const char *msg)//mutex?
+int	ft_strlen(const char *str)
 {
-	printf("Error: %s\n", msg);
+	int	count;
+
+	count = 0;    
+	while (str[count])
+		count++;
+	return (count);
+}
+//not needed mutex here
+void	print_error_msg(const char *msg)
+{
+	const char	*prefix;
+
+	prefix = "Error: ";   
+	write(STDERR_FILENO, prefix, ft_strlen(prefix));
+	write(STDERR_FILENO, msg, ft_strlen(msg));
+	write(STDERR_FILENO, "\n", 1);
+	// printf("Error: %s\n", msg);
 }
