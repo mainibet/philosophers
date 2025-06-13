8bd48a3f7b7eef9ae2a4302ecaf2954b353df4d0
Alicia Betancourt Mainieri <albetanc@c4a7c8.42berlin.de>
2025-06-13 17:43:19 +0200

    refactored life_monitor with helper function

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 08:00:55 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/10 13:48:28 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/13 17:43:03 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 //if receives program it will only work with philo[0]
 //with param philo it can check all
 //check time from last meal
-static int check_life(t_philo *philo)
+static int	check_life(t_philo *philo)
 {
 	long long	since_last_meal;
 	int			philo_status;
@@ -24,8 +24,8 @@ static int check_life(t_philo *philo)
 	pthread_mutex_lock(&philo->philo_mutex);
 	since_last_meal = precise_time_ms() - philo->last_meal;
 	if (since_last_meal >= philo->program->time_die)
-		philo_status = PHILO_DIED;//needs to be in the struct?
-	pthread_mutex_unlock(&philo->philo_mutex);//unlock after reading
+		philo_status = PHILO_DIED;
+	pthread_mutex_unlock(&philo->philo_mutex);
 	return (philo_status);
 }
 
@@ -36,7 +36,7 @@ static int	meal_control(t_program *data)
 	int	i;
 
 	if (data->max_meals == MAX_MEALS_DISABLED)
-		return(PHILO_ALIVED);
+		return (PHILO_ALIVED);
 	i = 0;
 	while (i < data->total_philo)
 	{
@@ -52,6 +52,22 @@ static int	meal_control(t_program *data)
 	return (PHILO_DIED);
 }
 
+static int	check_all_philos(t_program *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->total_philo)
+	{
+		if (check_life(&data->philo[i]) == PHILO_DIED)
+		{
+			end_program(data, "died", &data->philo[i]);
+			return (PHILO_DIED);
+		}
+		i++;
+	}
+	return (PHILO_ALIVED);
+}
 //monitores the philo's lifes
 //CONNECT WITH THE EAT LOGIC and end condition with the end_flag
 //will trigger the programm termination
@@ -62,35 +78,38 @@ static int	meal_control(t_program *data)
 //usleep: small initial delay to help ensure all philo threads start
 // Loop until the end_flag is set (simulation termination condition met)
 // Loop infinitely, breaking out based on end_flag
-void	*life_monitor(void *arg)//check why adn how to connect it good with all
-{//what if monito fails? or not need to check it?
-	int			i;
+//when return (NULL) is ending the monitor thread
+void	*life_monitor(void *arg)
+{
+	// int			i;
 	t_program	*data;
-    int			life_status;
+	int			life_status;
 
-	data = (t_program *) arg;//check if needed usleep
+	data = (t_program *) arg;
 	usleep(1000);
 	while (1)
 	{
-		i = 0;
-		life_status = check_end_cond(&data->philo[0]);//check if will check all philos and not only philo[0]
+		// i = 0;
+		life_status = check_end_cond(&data->philo[0]);
 		if (life_status == PHILO_DIED)
-			return (NULL);//CAN I RETURN AFTER UNLOCK this is to exit monitor thread
-		while (i < data->total_philo)// Check if any philosopher has died
+			return (NULL);
+		if (check_all_philos(data) == PHILO_DIED)
+			return (NULL);
+		// while (i < data->total_philo)
+		// {
+		// 	if (check_life(&data->philo[i]) == PHILO_DIED)
+		// 	{
+		// 		end_program(data, "died", &data->philo[i]);
+		// 		return (NULL);
+		// 	}
+		// 	i++;
+		// }
+		if (data->max_meals != MAX_MEALS_DISABLED 
+			&& meal_control(data) == PHILO_DIED)
 		{
-			if (check_life(&data->philo[i]) == PHILO_DIED)
-			{
-				end_program(data, "died", &data->philo[i]);
-				return (NULL);//terminates monitor thread
-			}
-			i++;
-		}
-		if (data->max_meals != MAX_MEALS_DISABLED && meal_control(data) == PHILO_DIED)
-		{
-			end_program(data, "All philos have eaten all set meals", NULL);//no message asked, can I include some?
+			end_program(data, "All philos have eaten all set meals", NULL);
 			return (NULL);
 		}
-		usleep(100);//can be modified 1000
+		usleep(100);
 	}
-	return (NULL);//Should be unreachable if return statements are used inside loop
 }
