6b2b2c4f4ff8d2c35ff8f66273fc9ccc6c2123ea
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-06-16 10:08:40 +0200

    norm happy with life_cycle

--- philo/life_cycle.c
+++ philo/life_cycle.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 07:49:31 by albetanc          #+#    #+#             */
-/*   Updated: 2025/06/13 17:10:15 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/06/16 10:08:07 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ static void	philo_routine_even(t_philo *philo)
 		return ;
 }
 
+//check if specific philo reached max meals
 static void	run_life(t_philo *philo)
 {
 	while (1)
@@ -54,7 +55,7 @@ static void	run_life(t_philo *philo)
 			philo_routine_even(philo);
 		if (check_end_cond(philo) == PHILO_DIED)
 			break ;
-		pthread_mutex_lock(&philo->philo_mutex);//check if specific philo reached max meals
+		pthread_mutex_lock(&philo->philo_mutex);
 		if (philo->program->max_meals != MAX_MEALS_DISABLED
 			&& philo->meal_number >= philo->program->max_meals)
 		{
@@ -63,18 +64,16 @@ static void	run_life(t_philo *philo)
 		}
 		pthread_mutex_unlock(&philo->philo_mutex);
 	}
-	// 	if (philo->forks_taken > 0)//new all this if cond
-	// {
-	// 	release_forks(philo);
-	// 	philo->forks_taken = 0;
-	// }
 	if (philo->lfork_status == TAKEN_FORK || philo->rfork_status == TAKEN_FORK)
 		release_forks(philo);
 }
+
 //init last meal and meal number
 //take only one fork
 //wait until monitor detects its dead
 //release the fork before dying
+// when Usleep is released CPU
+//at the end relseased fork when program ends
 void	*life_one_philo(void *arg)
 {
 	t_philo	*philo;
@@ -86,43 +85,37 @@ void	*life_one_philo(void *arg)
 	philo->meal_number = 0;
 	pthread_mutex_unlock(&philo->philo_mutex);
 	pthread_mutex_lock(&philo->left_fork->fork_mutex);
-	// philo->forks_taken = 1;//mark that was taken
-	set_fork_status(philo, philo->left_fork, TAKEN_FORK);//new
+	set_fork_status(philo, philo->left_fork, TAKEN_FORK);
 	print_status(philo, "has taken a fork");
 	while (check_end_cond(philo) == PHILO_ALIVED)
-		usleep(1000); // release CPU
-	pthread_mutex_unlock(&philo->left_fork->fork_mutex); // Libera el tenedor al morir/terminar
-	// philo->forks_taken = 0;//reset
-	set_fork_status(philo, philo->left_fork, NO_TAKEN);//new
+		usleep(1000);
+	pthread_mutex_unlock(&philo->left_fork->fork_mutex);
+	set_fork_status(philo, philo->left_fork, NO_TAKEN);
 	return (NULL);
 }
 
 //each will be a status
 //eating, thinking, and sleeping.
-//prototype given by pthread_create
-//void *(*start_routine)(void *), void *arg
-//thread logic
-//odd: eat-sleep-think
-//even: think-eat-sleep
+//prototype given by pthread_create: void *(*start_routine)(void *), void *arg
+//this function handles the thread logic
+//odd philos with diff routine than even
 //After a full routine, check if max_meals is reached for this philo
-// (The monitor will check if *all* philos have reached it)
 // usleep (100) is to Yield CPU to avoid busy-waiting
-//steps:
-//2.  Initialize philosopher's state after global start time is set 
+//steps: sync imulation, set initial meal and be sure meal num is 0
 //This happens *after* all threads are ready and 'start_time' is reliable.
 //It's essential for the monitor to accurately track hunger from the beginning.
 //We lock the philo_mutex to safely update last_meal and meal_number.
-//3. Initial delay for even-ID philosophers to prevent immediate deadlock
+//Sync =  Initial delay for even-ID philosophers to prevent immediate deadlock
 void	*life_cycle(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
 	sync_simulation(philo);
-	pthread_mutex_lock(&philo->philo_mutex);//init philos
-	philo->last_meal = precise_time_ms(); // Set initial last_meal to global simulation start time
-	philo->meal_number = 0; // Ensure meal_number is 0 at the start of its life cycle
-	pthread_mutex_unlock(&philo->philo_mutex);//end init philos
+	pthread_mutex_lock(&philo->philo_mutex);
+	philo->last_meal = precise_time_ms();
+	philo->meal_number = 0;
+	pthread_mutex_unlock(&philo->philo_mutex);
 	usleep(100);
 	run_life(philo);
 	return (NULL);
